3440e7813da6aebb9dcb84b9bf8e59681f3b4b9b
caseymccullough <kcmccullough@gmail.com>
2021-05-17 09:20:54 -0400

    changed names within User schema to be compatible with client

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const userSchema = new Schema({
     username: { type: String, unique: true, required: true },
     password: { type: String, required: true },
     zipcode: {type: Number, required: true},
-    loads: {type: Array}
+    loads: {type: Array, required: true}
 })
 
 module.exports = model('User', userSchema)
